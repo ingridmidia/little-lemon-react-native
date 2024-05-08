2c85a37a6e6f4cfadd150e2fa6f6aca6f5a4bb25
ingridmidia <ingridmidia@gmail.com>
2024-05-08 10:43:34 -0500

    add logo image to header

--- components/LittleLemonHeader.js
+++ components/LittleLemonHeader.js
@@ -1,21 +1,33 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
 
 export default function LittleLemonHeader() {
   return (
     <View style={headerStyles.container}>
-      <Text style={headerStyles.headerText}>Little Lemon</Text>
+      <Image style={headerStyles.logo} source={require("../img/littleLemonLogo.png")} />
+      <Text style={headerStyles.headerText}>
+        Your Local Mediterranean Bistro
+      </Text>
     </View>
   );
 }
 
 const headerStyles = StyleSheet.create({
   container: {
-    backgroundColor: "#F4CE14",
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
+    
   },
   headerText: {
-    padding: 40,
-    fontSize: 30,
+    padding: 10,
+    fontSize: 18,
     color: "black",
     textAlign: "center",
+  },
+  logo: {
+    height: 100,
+    width: 300,
+    resizeMode: "contain",
+    marginTop: 20
   },
 });
