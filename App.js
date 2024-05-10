79158510aaefdfa9d86e360b2cc0dd98d0b64f0c
ingridmidia <ingridmidia@gmail.com>
2024-05-10 13:14:19 -0500

    add drawer navigation

--- App.js
+++ App.js
@@ -41,57 +41,106 @@
 // });
 
 //Tab Navigation
+// import { View, StyleSheet } from "react-native";
+// import LittleLemonHeader from "./components/LittleLemonHeader";
+// import WelcomeScreen from "./components/WelcomeScreen";
+// import Menu from "./components/Menu";
+// import LoginScreen from "./components/LoginScreen";
+// import { NavigationContainer } from "@react-navigation/native";
+// import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+// import { MaterialIcons } from "@expo/vector-icons";
+
+// const Tab = createBottomTabNavigator();
+
+// export default function App() {
+//   return (
+//     <NavigationContainer>
+//       <View style={styles.container}>
+//         <LittleLemonHeader />
+//         <Tab.Navigator
+//           screenOptions={({ route }) => ({
+//             tabBarIcon: ({ focused, color, size }) => {
+//               let iconName;
+//               if (route.name === "Home") {
+//                 iconName = focused ? "home" : "home";
+//               } else if (route.name === "Menu") {
+//                 iconName = "restaurant-menu";
+//               } else {
+//                 iconName = "login";
+//               }
+//               return (
+//                 <MaterialIcons name={iconName} size={size} color={color} />
+//               );
+//             },
+//             tabBarActiveTintColor: "tomato",
+//             tabBarInactiveTintColor: "gray",
+//           })}
+//         >
+//           <Tab.Screen
+//             name="Home"
+//             component={WelcomeScreen}
+//             options={{ headerShown: false }}
+//           />
+//           <Tab.Screen
+//             name="Menu"
+//             component={Menu}
+//             options={{ headerShown: false }}
+//           />
+//           <Tab.Screen
+//             name="Login"
+//             component={LoginScreen}
+//             options={{ headerShown: false }}
+//           />
+//         </Tab.Navigator>
+//       </View>
+//     </NavigationContainer>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#495E57",
+//   },
+//   footer: {
+//     backgroundColor: "#495E57",
+//   },
+// });
+
+//Drawer Navigation
 import { View, StyleSheet } from "react-native";
 import LittleLemonHeader from "./components/LittleLemonHeader";
 import WelcomeScreen from "./components/WelcomeScreen";
 import Menu from "./components/Menu";
 import LoginScreen from "./components/LoginScreen";
 import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { Ionicons, Entypo, MaterialIcons } from "@expo/vector-icons";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 
-const Tab = createBottomTabNavigator();
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
       <View style={styles.container}>
         <LittleLemonHeader />
-        <Tab.Navigator
-          screenOptions={({ route }) => ({
-            tabBarIcon: ({ focused, color, size }) => {
-              let iconName;
-              if (route.name === "Home") {
-                iconName = focused ? "home" : "home";
-              } else if (route.name === "Menu") {
-                iconName = "restaurant-menu";
-              } else {
-                iconName = "login";
-              }
-              return (
-                <MaterialIcons name={iconName} size={size} color={color} />
-              );
-            },
-            tabBarActiveTintColor: "tomato",
-            tabBarInactiveTintColor: "gray",
-          })}
+        <Drawer.Navigator initialRouteName="Home"
         >
-          <Tab.Screen
+          <Drawer.Screen
             name="Home"
             component={WelcomeScreen}
-            options={{ headerShown: false }}
+            
           />
-          <Tab.Screen
+          <Drawer.Screen
             name="Menu"
             component={Menu}
-            options={{ headerShown: false }}
+           
           />
-          <Tab.Screen
+          <Drawer.Screen
             name="Login"
             component={LoginScreen}
-            options={{ headerShown: false }}
+           
           />
-        </Tab.Navigator>
+        </Drawer.Navigator>
       </View>
     </NavigationContainer>
   );
