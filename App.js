25991fd53a0c9980dccff9aec9e09ec4a6d148a5
ingridmidia <ingridmidia@gmail.com>
2024-05-10 11:35:15 -0500

    add icons to tab navigation

--- App.js
+++ App.js
@@ -47,8 +47,8 @@ import WelcomeScreen from "./components/WelcomeScreen";
 import Menu from "./components/Menu";
 import LoginScreen from "./components/LoginScreen";
 import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator} from "@react-navigation/bottom-tabs";
-import { Iconicons} from "@expo/vector-icons";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons, Entypo, MaterialIcons } from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator();
 
@@ -57,10 +57,40 @@ export default function App() {
     <NavigationContainer>
       <View style={styles.container}>
         <LittleLemonHeader />
-        <Tab.Navigator>
-          <Tab.Screen name="Home" component={WelcomeScreen} />
-          <Tab.Screen name="Menu" component={Menu} />
-          <Tab.Screen name="Login" component={LoginScreen} />
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName;
+              if (route.name === "Home") {
+                iconName = focused ? "home" : "home";
+              } else if (route.name === "Menu") {
+                iconName = "restaurant-menu";
+              } else {
+                iconName = "login";
+              }
+              return (
+                <MaterialIcons name={iconName} size={size} color={color} />
+              );
+            },
+            tabBarActiveTintColor: "tomato",
+            tabBarInactiveTintColor: "gray",
+          })}
+        >
+          <Tab.Screen
+            name="Home"
+            component={WelcomeScreen}
+            options={{ headerShown: false }}
+          />
+          <Tab.Screen
+            name="Menu"
+            component={Menu}
+            options={{ headerShown: false }}
+          />
+          <Tab.Screen
+            name="Login"
+            component={LoginScreen}
+            options={{ headerShown: false }}
+          />
         </Tab.Navigator>
       </View>
     </NavigationContainer>
